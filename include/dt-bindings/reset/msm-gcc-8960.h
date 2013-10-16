3685d1f6f843cd1da314a0756096046462e33a35
Stephen Boyd <sboyd@codeaurora.org>
2013-10-16 00:40:12 -0700

    clk: msm: Add support for MSM8960's global clock controller (GCC)
    Add a driver for the global clock controller found on MSM8960
    based platforms. This should allow most non-multimedia device
    drivers to probe and control their clocks.
    
    Cc: <devicetree@vger.kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

---
+++ include/dt-bindings/reset/msm-gcc-8960.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_RESET_MSM_GCC_8960_H
+#define _DT_BINDINGS_RESET_MSM_GCC_8960_H
+
+#define AFAB_SMPSS_S_RESET				0
+#define AFAB_SMPSS_M1_RESET				1
+#define AFAB_SMPSS_M0_RESET				2
+#define ADM0_C2_RESET					3
+#define ADM0_C1_RESET					4
+#define ADM0_C0_RESET					5
+#define ADM0_PBUS_RESET					6
+#define ADM0_RESET					7
+#define QDSS_CLKS_SW_ASYNC_RESET			8
+#define QDSS_POR_ASYNC_RESET				9
+#define QDSS_TSCTR_ASYNC_RESET				10
+#define QDSS_HRESET_ASYNC_RESET				11
+#define QDSS_AXI_ASYNC_RESET				12
+#define QDSS_DBG_ASYNC_RESET				13
+#define SFAB_PCIE_M_RESET				14
+#define SFAB_PCIE_S_RESET				15
+#define PPSS_PROC_RESET					16
+#define PPSS_RESET					17
+#define SDC1_RESET					18
+#define SDC2_RESET					19
+#define SDC3_RESET					20
+#define SDC4_RESET					21
+#define SDC5_RESET					22
+#define USB_HS1_RESET					23
+#define USB_HSIC_RESET					24
+#define USB_FS1_XCVR_RESET				25
+#define USB_FS1_RESET					26
+#define USB_FS2_XCVR_RESET				27
+#define USB_FS2_RESET					28
+#define GSBI1_RESET					29
+#define GSBI2_RESET					30
+#define GSBI3_RESET					31
+#define GSBI4_RESET					32
+#define GSBI5_RESET					33
+#define GSBI6_RESET					34
+#define GSBI7_RESET					35
+#define GSBI8_RESET					36
+#define GSBI9_RESET					37
+#define GSBI10_RESET					38
+#define GSBI11_RESET					39
+#define GSBI12_RESET					40
+#define SPDM_RESET					41
+#define MSS_RESET					42
+#define MPM_RESET					43
+#define RIVA_RESET					44
+
+#endif
