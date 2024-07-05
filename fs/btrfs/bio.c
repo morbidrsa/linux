87d0369d0ec50d49767701190d3070d16a9fb7f0
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2024-07-05 11:15:17 +0200

    btrfs: stripe-tree: also look at commit root on relocation
    When doing reads on a raid stripe tree enabled system while relocation
    is ongoinig, also look at the commit root. The data we're interested
    in could've just been relocated to another place in the current
    transaction.
    
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>

--- fs/btrfs/bio.c
+++ fs/btrfs/bio.c
@@ -679,7 +679,8 @@ static bool btrfs_submit_chunk(struct btrfs_bio *bbio, int mirror_num)
 	blk_status_t ret;
 	int error;
 
-	smap.commit_root = !bbio->inode;
+	smap.commit_root = !bbio->inode ||
+		btrfs_is_data_reloc_root(inode->root);
 
 	btrfs_bio_counter_inc_blocked(fs_info);
 	error = btrfs_map_block(fs_info, btrfs_op(bio), logical, &map_length,
